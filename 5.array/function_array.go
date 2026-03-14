24572351f6edd05af84b8314d865367470964492
BenArief <benarief29@gmail.com>
2026-03-14 23:37:45 +0700

    Update little

--- 5.array/function_array.go
+++ 5.array/function_array.go
@@ -19,4 +19,8 @@ func main() {
 	arr2 := [3]int{4,5,6}
 	fmt.Println("arr1 == arr2 : ", arr1==arr2)
 	fmt.Println("arr1 != arr2 : ", arr1!=arr2)
+
+	if(arr1 != arr2){
+		fmt.Println("arr1 dan arr2 berbeda")
+	}
 }
